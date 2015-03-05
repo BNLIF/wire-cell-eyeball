837abdd4da8d3ee5b60bebefd3540582f09e031d
Brett Viren <brett.viren@gmail.com>
2015-03-05 11:14:18 -0500

    First commit

---
+++ wires.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+name=$1 ; shift
+if [ ! -f ${name}.euk ] ; then
+    echo "No such file or directory" 1>&2
+    exit 1
+fi
+rm -f ${name}.{ps,png,svg}
+eukleides -o${name}.ps ${name}.euk
+inkscape --without-gui --file=${name}.ps --export-area-drawing \
+    --export-pdf=${name}.pdf \
+    --export-png=${name}.png >/dev/null
+
